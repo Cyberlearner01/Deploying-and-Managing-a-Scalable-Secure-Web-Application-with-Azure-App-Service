f51a750e6ad7616fde93910321d125a50251b0db
Cyberjay <flexjohnson26@gmail.com>
2025-03-12 12:48:16 -0400

    Add Terraform configuration for Azure infrastructure

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,17 +12,16 @@ provider "azurerm" {
   subscription_id = "d07e93af-325f-4672-a73b-124502a553dc"
 }
 
-
-
 resource "azurerm_resource_group" "project-1" {
   name     = "Project-rg"
   location = "Canada Central"
 }
 
+# Add the service plan resource
 resource "azurerm_service_plan" "app_plan" {
   name                = "Johnson-plan"
-  location            = azurerm_resource_group.project-1.location
   resource_group_name = azurerm_resource_group.project-1.name
+  location            = azurerm_resource_group.project-1.location
   os_type             = "Linux"
   sku_name            = "P1v3"
 }
@@ -81,25 +80,132 @@ resource "azurerm_role_assignment" "github_identity_role" {
 }
 
 resource "azurerm_app_service_source_control" "webapp_source_control" {
-  app_id                 = azurerm_linux_web_app.webapp.id
-  repo_url               = "https://github.com/Cyberlearner01/Deploying-and-Managing-a-Scalable-Secure-Web-Application-with-Azure-App-Service.git"
+  app_id   = azurerm_linux_web_app.webapp.id
+  repo_url = "https://github.com/Cyberlearner01/Deploying-and-Managing-a-Scalable-Secure-Web-Application-with-Azure-App-Service"
+
   branch                 = "main"
   use_manual_integration = false
 }
 
-
 resource "azurerm_app_service_custom_hostname_binding" "custom_domain" {
-  hostname            = "www.durojohnson.com" # Ensure this is your actual domain
+  hostname            = "www.durojohnson.com"
   app_service_name    = azurerm_linux_web_app.webapp.name
-  resource_group_name = azurerm_resource_group.project-1.name # Ensure correct variable name
-}
-
-resource "azurerm_app_service_managed_certificate" "ssl" {
-  custom_hostname_binding_id = azurerm_app_service_custom_hostname_binding.custom_domain.id
+  resource_group_name = azurerm_resource_group.project-1.name
 }
 
 resource "azurerm_app_service_certificate_binding" "ssl_binding" {
   hostname_binding_id = azurerm_app_service_custom_hostname_binding.custom_domain.id
-  certificate_id      = azurerm_app_service_managed_certificate.ssl.id
+  certificate_id      = "/subscriptions/d07e93af-325f-4672-a73b-124502a553dc/resourceGroups/Project-rg/providers/Microsoft.Web/certificates/www.durojohnson.com-jaykxr-webapp"
   ssl_state           = "SniEnabled"
+}
+
+# Adding the auto-scaling setting
+resource "azurerm_monitor_autoscale_setting" "webscale-rg" {
+  enabled             = true
+  location            = "canadacentral"
+  name                = "Mywebscaling"
+  resource_group_name = azurerm_resource_group.project-1.name
+  target_resource_id  = azurerm_service_plan.app_plan.id
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {
+      default = 1
+      maximum = 10
+      minimum = 1
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_namespace   = "microsoft.web/serverfarms"
+        metric_resource_id = azurerm_service_plan.app_plan.id
+        operator           = "GreaterThan"
+        statistic          = "Average"
+        threshold          = 75
+        time_aggregation   = "Average"
+        time_grain         = "PT1M"
+        time_window        = "PT5M"
+      }
+
+      scale_action {
+        cooldown  = "PT5M"
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = 1
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_namespace   = "Microsoft.Web/serverfarms"
+        metric_resource_id = azurerm_service_plan.app_plan.id
+        operator           = "LessThan"
+        statistic          = "Average"
+        threshold          = 25
+        time_aggregation   = "Average"
+        time_grain         = "PT1M"
+        time_window        = "PT5M"
+      }
+
+      scale_action {
+        cooldown  = "PT5M"
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = 1
+      }
+    }
+  }
+}
+
+
+# 1. Create a Storage Account for Logs and Backups
+resource "azurerm_storage_account" "jaykxrstore" {
+  name                     = "jaykxrstore"  # Globally unique name
+  resource_group_name      = azurerm_resource_group.project-1.name
+  location                 = azurerm_resource_group.project-1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# 2. Create a Log Analytics Workspace
+resource "azurerm_log_analytics_workspace" "webapp_log_analytics" {
+  name                = "webapp-log-analytics"
+  resource_group_name = azurerm_resource_group.project-1.name
+  location            = azurerm_resource_group.project-1.location
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# 3. Create Application Insights
+resource "azurerm_application_insights" "webapp_insights" {
+  name                = "webapp-insights"
+  location            = azurerm_resource_group.project-1.location
+  resource_group_name = azurerm_resource_group.project-1.name
+  application_type    = "web"
+}
+
+# 4. Create a Backup Vault and Backup Policy
+resource "azurerm_recovery_services_vault" "webapp_backup_vault" {
+  name                = "webapp-backup-vault"
+  resource_group_name = azurerm_resource_group.project-1.name
+  location            = azurerm_resource_group.project-1.location
+  sku                 = "Standard"
+  }
+
+resource "azurerm_backup_policy_vm" "daily_backup_policy" {
+  name                = "daily-backup-policy"
+  resource_group_name = azurerm_resource_group.project-1.name
+  recovery_vault_name = azurerm_recovery_services_vault.webapp_backup_vault.name
+
+  backup {
+    frequency = "Daily"
+    time      = "23:00"  # Backup time (adjust as needed)
+  }
+
+  retention_daily {
+    count = 30  # Retention period in days
+  }
 }
